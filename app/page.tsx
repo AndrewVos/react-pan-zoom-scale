d6a5badb469ddea64038a737dc4a7d912a4b53b4
Andrew Vos <andrew.vos@gmail.com>
2024-04-24 14:21:03 +0100

    Title change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <main className="h-full overflow-hidden touch-none container mx-auto p-3 flex flex-col gap-3" >
-      <h1 className="text-3xl font-bold text-gray-900">React two finger pan zoom and pinch</h1>
+      <h1 className="text-3xl font-bold text-gray-900">Two finger pan zoom and pinch in React</h1>
       <div className="text-lg text-red-700">
         This only works on touch devices.
       </div>
